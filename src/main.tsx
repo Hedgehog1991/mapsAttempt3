6720e25c229133b1932e7d37295a9db50be684d2
Hedgehog1991 <kriive@icloud.com>
2024-01-19 16:44:57 +0100

    trying to fix main

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>Hello Reacts</h1>);
+root.render(<h1>Hello Reacts world</h1>);
